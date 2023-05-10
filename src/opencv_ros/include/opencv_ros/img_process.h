c5bb3eddf11206dab6d2e94f31e3169261f40c3d
girlrush <lakms123456@naver.com>
2023-05-10 07:19:11 +0000

    Get depth from stereo

--- src/opencv_ros/include/opencv_ros/img_process.h
+++ src/opencv_ros/include/opencv_ros/img_process.h
@@ -11,36 +11,50 @@
 
 #pragma once
 
+struct ParamStereo{
+    int num_disparities     = 8;
+    int block_size          = 5;
+    int pre_filter_type     = 1;
+    int pre_filter_size     = 1;
+    int pre_filter_cap      = 31;
+    int min_disparity       = 0;
+    int texture_threshold   = 10;
+    int uniqueness_ratio    = 15;
+    int speckle_range       = 0;
+    int speckle_window_size = 0;
+    int disp12_max_diff     = -1;
+    int dis_type            = CV_16S;
+};
+
 class ImageProc
 {
 private:
     ros::NodeHandle nh;
 
-    ros::Publisher pub_procImg_canny;
-    ros::Publisher pub_procImg_hough;
-    ros::Publisher pub_procImg_keypoint;
-    
-    // ros::Subscriber sub_left_image;
-    // ros::Subscriber sub_right_image;
+    ros::Publisher pub_canny;
+    ros::Publisher pub_hough;
+    ros::Publisher pub_disparity;
 
+    ros::Publisher pub_left_nice;
+    ros::Publisher pub_right_nice;
+
+    
     int gaussian_min; //0~360
     int gaussian_max; //0~360
 
-    cv::Mat std_image;
-    cv::Mat std_descriptors;
-    std::vector<cv::KeyPoint> std_keypoint;
-    cv::Ptr<cv::Feature2D> std_f2d = cv::xfeatures2d::SURF::create();  
 
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_policy_stereo;
     typedef std::shared_ptr<message_filters::Synchronizer<sync_policy_stereo>> synchronizer_stereo;
+
     std::shared_ptr<message_filters::Subscriber<sensor_msgs::Image>> sub_left_image;
     std::shared_ptr<message_filters::Subscriber<sensor_msgs::Image>> sub_right_image;
 
     synchronizer_stereo sync_stereo;
 
-    void ReadParam(ros::NodeHandle& node_);
-    // void cb_image(const sensor_msgs::ImageConstPtr& msg);
+    ParamStereo param_stereo;
+    cv::Ptr<cv::StereoBM> stereo = cv::StereoBM::create();
 
+    void ReadParam(ros::NodeHandle& node_);
     void CallbackStereo(const sensor_msgs::ImageConstPtr& , const sensor_msgs::ImageConstPtr&);
 
 public:
--- src/opencv_ros/src/img_process.cpp
+++ src/opencv_ros/src/img_process.cpp
@@ -4,43 +4,126 @@ void ImageProc::init(ros::NodeHandle& node_)
 {
     nh = node_;
     
-    ReadParam(node_);
+    ReadParam(nh);
 
-    pub_procImg_canny = nh.advertise<sensor_msgs::Image>("/output/canny", 10);
-    pub_procImg_hough = nh.advertise<sensor_msgs::Image>("/output/hough", 10);
+    pub_canny = nh.advertise<sensor_msgs::Image>("/output/canny", 10);
+    pub_hough = nh.advertise<sensor_msgs::Image>("/output/hough", 10);
+    pub_disparity = nh.advertise<sensor_msgs::Image>("/output/disparity", 10);
 
-    pub_procImg_keypoint = nh.advertise<sensor_msgs::Image>("/output/keypoint", 10);
-
-    // sub_left_image = nh.subscribe<sensor_msgs::Image>("/left_image_topic", 10, &ImageProc::cb_image, this);
-    // sub_right_image = nh.subscribe<sensor_msgs::Image>("/right_image_topic", 10, &ImageProc::cb_image, this);
+    pub_left_nice  = nh.advertise<sensor_msgs::Image>("/output/left_nice", 10);
+    pub_right_nice = nh.advertise<sensor_msgs::Image>("/output/right_nice", 10);
 
     sub_left_image.reset(new message_filters::Subscriber<sensor_msgs::Image>(nh, "/stereo/left", 10));
     sub_right_image.reset(new message_filters::Subscriber<sensor_msgs::Image>(nh, "/stereo/right", 10));
 
     sync_stereo.reset(new message_filters::Synchronizer<sync_policy_stereo>(sync_policy_stereo(100), *sub_left_image, *sub_right_image));
     sync_stereo->registerCallback(boost::bind(&ImageProc::CallbackStereo, this, _1, _2));
+    
 
 }
 
 void ImageProc::ReadParam(ros::NodeHandle& node_)
 {
-    if(!node_.getParam("gaussian_min", gaussian_min))
-        ROS_WARN("[Fail] get - gaussain_min");
-    else
-        ROS_INFO("[Param] gaussain_min : %d", gaussian_min);
+
+    nh.param("gaussian/min", gaussian_min, 0);
+    nh.param("gaussian/max", gaussian_max, 360);
 
 
-    if(!node_.getParam("gaussian_max", gaussian_max))
-        ROS_WARN("[Fail] get - gaussain_max");
-    else
-        ROS_INFO("[Param] gaussain_max : %d", gaussian_max);
+    std::cout << "Gaussian max = " << gaussian_max << std::endl;
+    std::cout << "Gaussian min = " << gaussian_min << std::endl;
     
 }
 
 void ImageProc::CallbackStereo(const sensor_msgs::ImageConstPtr& img_left, const sensor_msgs::ImageConstPtr& img_right)
 {
+    cv_bridge::CvImagePtr left_cv_ptr;
+    cv_bridge::CvImagePtr right_cv_ptr;
+
+    cv_bridge::CvImage left_cv_proc;
+    cv_bridge::CvImage right_cv_proc;
+
+    cv_bridge::CvImage disparity_cv_proc;
+
+    try
+    {
+        // Convert sensor_msgs to CvImage
+        left_cv_ptr  = cv_bridge::toCvCopy(img_left, sensor_msgs::image_encodings::RGB8);
+        right_cv_ptr = cv_bridge::toCvCopy(img_right, sensor_msgs::image_encodings::RGB8);
+
+        std::cout << "Left image : " << left_cv_ptr->image.size() << std::endl;
+        std::cout << "Right image : " << right_cv_ptr->image.size() << std::endl;
+    }
+    catch (cv_bridge::Exception& e) { std::cout << "Fail to convert images" << std::endl; return;}
+
+    // Change color to gray scale
+    cv::cvtColor (left_cv_ptr->image, left_cv_proc.image, cv::COLOR_RGB2GRAY);
+    cv::cvtColor (right_cv_ptr->image, right_cv_proc.image, cv::COLOR_RGB2GRAY);
+
+    left_cv_ptr->encoding = sensor_msgs::image_encodings::MONO8;
+    right_cv_ptr->encoding = sensor_msgs::image_encodings::MONO8;
+
+    cv::Mat disp(left_cv_ptr->image.size(), CV_32FC1), disparity(left_cv_ptr->image.size(), CV_32FC1);
+
+    cv::Mat left_stereo_map1(left_cv_ptr->image.size(), CV_32FC1), left_stereo_map2(left_cv_ptr->image.size(), CV_32FC1);
+    cv::Mat right_stereo_map1(right_cv_ptr->image.size(), CV_32FC1), right_stereo_map2(right_cv_ptr->image.size(), CV_32FC1);
+
+
+    cv::Mat left_nice, right_nice;
+
+    cv::remap(left_cv_proc.image,
+        left_nice,
+        left_stereo_map1,
+        left_stereo_map2,
+        cv::INTER_LANCZOS4,
+        cv::BORDER_CONSTANT,
+        0);
+
+
+    cv::remap(right_cv_proc.image,
+        right_nice,
+        right_stereo_map1,
+        right_stereo_map2,
+        cv::INTER_LANCZOS4,
+        cv::BORDER_CONSTANT,
+        0);
     
-}
+    // stereo->compute(left_nice, right_nice, disp);
+    stereo->compute(left_cv_proc.image, right_cv_proc.image, disp);
+
+    disp.convertTo(disparity, CV_8UC1);
+    
+    cv::normalize(disparity, disparity, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+    
+    // disp.convertTo(disparity, CV_32F, 1.0);
+    // disparity = (disparity/16.0f - (float)param_stereo.min_disparity / (float)param_stereo.num_disparities);
+
+    disparity.copyTo(disparity_cv_proc.image);
+    disparity_cv_proc.encoding = sensor_msgs::image_encodings::MONO8;
+
+    pub_disparity.publish(disparity_cv_proc);
+
+
+
+    // cv_bridge::CvImage left_nice_cv;
+    // cv_bridge::CvImage right_nice_cv;
+
+    // left_nice.copyTo(left_nice_cv.image);
+    // right_nice.copyTo(right_nice_cv.image);
+    
+    // left_nice_cv.encoding = sensor_msgs::image_encodings::RGB8;
+    // right_nice_cv.encoding = sensor_msgs::image_encodings::RGB8;
+
+    // pub_left_nice.publish(left_nice_cv);
+    // pub_right_nice.publish(right_nice_cv);
+
+
+    left_nice.copyTo(left_cv_proc.image);
+    right_nice.copyTo(right_cv_proc.image);
+
+    pub_left_nice.publish(left_cv_proc);
+    pub_right_nice.publish(right_cv_proc);
+    
+}   
 
 // void ImageProc::cb_image(const sensor_msgs::ImageConstPtr& msg)
 // {
