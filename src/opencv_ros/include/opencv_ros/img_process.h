08218cd1bfd21c36e62d922dc9f0e80811c86f7f
girlrush <lakms123456@naver.com>
2023-05-10 02:18:04 +0000

    stereo subscriber init

--- src/opencv_ros/include/opencv_ros/img_process.h
+++ src/opencv_ros/include/opencv_ros/img_process.h
@@ -1,6 +1,10 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/time_synchronizer.h>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d.hpp>
@@ -15,7 +19,9 @@ private:
     ros::Publisher pub_procImg_canny;
     ros::Publisher pub_procImg_hough;
     ros::Publisher pub_procImg_keypoint;
-    ros::Subscriber sub_image;
+    
+    // ros::Subscriber sub_left_image;
+    // ros::Subscriber sub_right_image;
 
     int gaussian_min; //0~360
     int gaussian_max; //0~360
@@ -25,9 +31,17 @@ private:
     std::vector<cv::KeyPoint> std_keypoint;
     cv::Ptr<cv::Feature2D> std_f2d = cv::xfeatures2d::SURF::create();  
 
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_policy_stereo;
+    typedef std::shared_ptr<message_filters::Synchronizer<sync_policy_stereo>> synchronizer_stereo;
+    std::shared_ptr<message_filters::Subscriber<sensor_msgs::Image>> sub_left_image;
+    std::shared_ptr<message_filters::Subscriber<sensor_msgs::Image>> sub_right_image;
+
+    synchronizer_stereo sync_stereo;
 
     void ReadParam(ros::NodeHandle& node_);
-    void cb_image(const sensor_msgs::ImageConstPtr& msg);
+    // void cb_image(const sensor_msgs::ImageConstPtr& msg);
+
+    void CallbackStereo(const sensor_msgs::ImageConstPtr& , const sensor_msgs::ImageConstPtr&);
 
 public:
     ImageProc(){ }
--- src/opencv_ros/src/img_process.cpp
+++ src/opencv_ros/src/img_process.cpp
@@ -11,20 +11,14 @@ void ImageProc::init(ros::NodeHandle& node_)
 
     pub_procImg_keypoint = nh.advertise<sensor_msgs::Image>("/output/keypoint", 10);
 
-    sub_image = nh.subscribe<sensor_msgs::Image>("/image_topic", 10, &ImageProc::cb_image, this);
+    // sub_left_image = nh.subscribe<sensor_msgs::Image>("/left_image_topic", 10, &ImageProc::cb_image, this);
+    // sub_right_image = nh.subscribe<sensor_msgs::Image>("/right_image_topic", 10, &ImageProc::cb_image, this);
 
+    sub_left_image.reset(new message_filters::Subscriber<sensor_msgs::Image>(nh, "/stereo/left", 10));
+    sub_right_image.reset(new message_filters::Subscriber<sensor_msgs::Image>(nh, "/stereo/right", 10));
 
-    //std::cout << "standard image process" << std::endl;
-    //std_image = cv::imread("/root/opencv_ws/src/opencv_ros/frame1473.jpg", 0);
-    
-    //std::cout << "standard image size : " << std_image.size() << std::endl;
-
-    //cv::GaussianBlur(std_image, std_image, cv::Size(3,3), 0);
-
-    //cv::Canny(std_image, std_image, gaussian_min, gaussian_max);
-
-    //std_f2d->detect(std_image, std_keypoint);
-    //std_f2d->compute(std_image, std_keypoint, std_descriptors);
+    sync_stereo.reset(new message_filters::Synchronizer<sync_policy_stereo>(sync_policy_stereo(100), *sub_left_image, *sub_right_image));
+    sync_stereo->registerCallback(boost::bind(&ImageProc::CallbackStereo, this, _1, _2));
 
 }
 
@@ -43,49 +37,54 @@ void ImageProc::ReadParam(ros::NodeHandle& node_)
     
 }
 
-void ImageProc::cb_image(const sensor_msgs::ImageConstPtr& msg)
+void ImageProc::CallbackStereo(const sensor_msgs::ImageConstPtr& img_left, const sensor_msgs::ImageConstPtr& img_right)
 {
-    std::cout << "Start image callback" << std::endl;
-    cv_bridge::CvImagePtr cv_ptr;
-    cv_bridge::CvImage cv_proc;
-
-    try
-    {
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        return;
-    }
-
-    std::cout << "Image Size : " << cv_ptr->image.size() << std::endl;
-
-    cv::cvtColor(cv_ptr->image, cv_proc.image, cv::COLOR_RGB2GRAY);
-    cv::GaussianBlur(cv_proc.image, cv_proc.image, cv::Size(3,3), 0);
-
-    cv::Canny(cv_proc.image, cv_proc.image, gaussian_min, gaussian_max);
-
-    cv::Ptr<cv::Feature2D> f2d = cv::xfeatures2d::SURF::create();  
-
-
-    cv_proc.encoding = sensor_msgs::image_encodings::MONO8;
-    pub_procImg_canny.publish(cv_proc.toImageMsg());
-
-    std::vector<cv::Vec4i> lines;
-
-    cv::HoughLinesP(cv_proc.image, lines, 1, CV_PI/180, 80, 10, 250);
-
-    // cv::Mat img_temp(cv_ptr->image.size(), CV_8UC3, cv::Scalar(0,0,0));
-    cv::Mat img_temp = cv_ptr->image.clone();
     
-    for (std::size_t i=0; i<lines.size(); i++)
-    {
-        cv::Vec4i l = lines[i];
-        cv::line(img_temp, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255, 255, 255), 1, cv::LINE_AA);
-    }
+}
 
-    cv_proc.encoding = sensor_msgs::image_encodings::RGB8;
-    cv_proc.image = img_temp.clone();
-    pub_procImg_hough.publish(cv_proc.toImageMsg());
+// void ImageProc::cb_image(const sensor_msgs::ImageConstPtr& msg)
+// {
+//     std::cout << "Start image callback" << std::endl;
+//     cv_bridge::CvImagePtr cv_ptr;
+//     cv_bridge::CvImage cv_proc;
 
-}   
+//     try
+//     {
+//         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
+//     }
+//     catch (cv_bridge::Exception& e)
+//     {
+//         return;
+//     }
+
+//     std::cout << "Image Size : " << cv_ptr->image.size() << std::endl;
+
+//     cv::cvtColor(cv_ptr->image, cv_proc.image, cv::COLOR_RGB2GRAY);
+//     cv::GaussianBlur(cv_proc.image, cv_proc.image, cv::Size(3,3), 0);
+
+//     cv::Canny(cv_proc.image, cv_proc.image, gaussian_min, gaussian_max);
+
+//     cv::Ptr<cv::Feature2D> f2d = cv::xfeatures2d::SURF::create();  
+
+
+//     cv_proc.encoding = sensor_msgs::image_encodings::MONO8;
+//     pub_procImg_canny.publish(cv_proc.toImageMsg());
+
+//     std::vector<cv::Vec4i> lines;
+
+//     cv::HoughLinesP(cv_proc.image, lines, 1, CV_PI/180, 80, 10, 250);
+
+//     // cv::Mat img_temp(cv_ptr->image.size(), CV_8UC3, cv::Scalar(0,0,0));
+//     cv::Mat img_temp = cv_ptr->image.clone();
+    
+//     for (std::size_t i=0; i<lines.size(); i++)
+//     {
+//         cv::Vec4i l = lines[i];
+//         cv::line(img_temp, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255, 255, 255), 1, cv::LINE_AA);
+//     }
+
+//     cv_proc.encoding = sensor_msgs::image_encodings::RGB8;
+//     cv_proc.image = img_temp.clone();
+//     pub_procImg_hough.publish(cv_proc.toImageMsg());
+
+// }   
